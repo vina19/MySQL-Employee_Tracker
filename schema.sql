87f3380d01a1b892f756190aba773c65ba3a6e88
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-24 19:47:18 -0600

    Created the employees database

---
+++ schema.sql
@@ -0,0 +1,9 @@
+/* Delete the database if there is database called employees_db before creating it. */
+DROP DATABASE IF EXISTS employees_db;
+
+/* Create the database employees_db. */
+CREATE DATABASE employees_db;
+
+/* Specify which database that we are going to use. */
+USE employees_db
+
