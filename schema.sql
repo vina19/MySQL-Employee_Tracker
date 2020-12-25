f3f26f4deb5b1e60cb6c2235a2db9b2cccd0b37d
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-24 20:04:32 -0600

    Created employee table

--- schema.sql
+++ schema.sql
@@ -24,3 +24,11 @@ CREATE TABLE role (
 );
 
 /* Employee Table */
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
