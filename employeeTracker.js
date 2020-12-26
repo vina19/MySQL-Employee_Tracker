8acd1ce8f4eebec31f7a7be920755791ee58eab7
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-25 20:09:18 -0600

    Created the connection for the sql database

--- employeeTracker.js
+++ employeeTracker.js
@@ -2,3 +2,14 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port, if not 3306
+    port: 3306,
+    // Your username, if not root
+    user: "root",
+    // Your password
+    password: "",
+    database: "employees_db"
+});
