056e4d60ca086e2061972ee4a8d5b51be2d435ad
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-02 20:34:25 -0600

    Added the foreign keys

--- schema.sql
+++ schema.sql
@@ -19,16 +19,20 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,2),
-    department_id INT,
-    PRIMARY KEY (id)
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 /* Employee Table */
+/* Foreign key : https://www.w3schools.com/sql/sql_foreignkey.asp */
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
