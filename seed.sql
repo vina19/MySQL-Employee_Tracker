774edc2ed6eb1a5311caae9ef32dafe769c4e3c1
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-02 20:24:10 -0600

    Added comments

--- seed.sql
+++ seed.sql
@@ -1,12 +1,14 @@
 /* Specify which database that we are going to use. */
 USE employees_db;
 
+/* Department */
 INSERT INTO department (name)
     VALUES ("Sales"),
            ("Engineering"),
            ("Finance"),
            ("Legal");
 
+/* Role - set the department_id based on the department id. */
 INSERT INTO role (title, salary, department_id)
     VALUES ("Sales Lead", 100000, 1),
            ("Salesperson", 80000, 1),
@@ -16,6 +18,7 @@ INSERT INTO role (title, salary, department_id)
            ("Legal Team Lead", 250000, 4),
            ("Lawyer", 190000, 4);
 
+/* Employee - set the role_id based on the role id and manager_id based on the employee id. */
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES ("John", "Doe", 1, 3),
            ("Mike", "Chan", 1, 1),
