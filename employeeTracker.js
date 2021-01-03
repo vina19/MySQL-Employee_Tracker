a2466d9a651a1826204d59e2ce59931948518378
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-02 22:16:53 -0600

    Added function to view all employees

--- employeeTracker.js
+++ employeeTracker.js
@@ -58,6 +58,8 @@ function runEmployeeTracker() {
                 'Exit'
             ]
         }).then(function(answer) {
+
+            // Condition for each options that the user choose.
             switch(answer.options) {
                 case 'View All Employees':
                     viewEmployees();
@@ -108,8 +110,15 @@ function runEmployeeTracker() {
         });
 };
 
+// Display all the employees data
 function viewEmployees() {
 
+    let query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON employee.manager_id = m.id";
+    connection.query(query, function(err, res) {
+        if (err) return err;
+        console.table(res);
+        runEmployeeTracker();
+    });
 };
 
 function viewEmployeesByDepartment() {
