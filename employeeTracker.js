d924de60ed04bb63272d6edb4e5871609ab96c36
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-03 21:46:52 -0600

    Using asciitable for displaying the employee by department

--- employeeTracker.js
+++ employeeTracker.js
@@ -156,7 +156,12 @@ function viewEmployeesByDepartment() {
             let query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON employee.manager_id = m.id WHERE department.name = '${answer.department}' ORDER BY employee.id`;
             connection.query(query, (err, res) => {
                 if (err) return err;
-                console.table(res);
+                let options = {
+                    skinny: true,
+                    intersectionCharacter: "x",
+                };
+                let table = asciitable(options, res);
+                console.log(table);
                 runEmployeeTracker();
             });
         });
